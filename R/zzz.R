6669a5a412111f8877681bf0ba0000c379c98b4f
Stephen J. Eglen <sje30@cam.ac.uk>
2013-05-09 12:26:24 +0000

    2013-05-09  Stephen Eglen  <stephen@gnu.org>
    	* R/zzz.R: Remove .First.lib() and use NAMESPACE instead to load
    	shared object.  Update .Last.lib() to .onUnload().

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,4 @@
-.First.lib <- function(lib, pkg)
-  library.dynam("sjevor", pkg, lib)
-
-.Last.lib <- function (libpath) {
+.onUnload <- function (libpath) {
   ## Run when the package is being unloaded.  This allows us to test
   ## packages within same session when dynlib is updated.
   library.dynam.unload("sjevor", libpath)
